de75f45cf50b252c7981d7c4a7c98acf7d530770
wxn0brP <wxn0brP@users.noreply.github.com>
2026-02-21 20:29:47 +0000

    deploy: 6c8deca7710af5af4833829d3b92762b87043b7e

--- zhiva-assets/api.js
+++ zhiva-assets/api.js
@@ -1,15 +1,15 @@
 (() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("secret");
+    const token = urlParams.get("zhiva-secret");
 
     window.fetchApi = (url, opts = {}, query = {}) => {
-        const urlObj = new URL("/api/" + url, window.location.origin);
+        const urlObj = new URL("/zhiva-api/" + url, window.location.origin);
 
         Object.entries(query).forEach(([key, value]) => {
             urlObj.searchParams.set(key, value);
         });
 
-        return fetch(urlObj.toString(), {
+        return fetch(urlObj, {
             ...opts,
             headers: {
                 ...opts.headers,
